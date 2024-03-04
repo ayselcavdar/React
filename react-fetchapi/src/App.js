1ba2484910134f6e7722bd007c5d6aeb33d543ad
Aysel Cavdar <a.misrikhanova13@gmail.com>
2024-03-04 21:44:48 +0400

    change app.js

--- react-fetchapi/src/App.js
+++ react-fetchapi/src/App.js
@@ -1,6 +1,61 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  const [name, setName] = useState("ice");
+  const [users, setUsers] = useState(false);
+  const [avatar, setAvatar] = useState(false);
+
+  const addPost = (data) => {
+    const headers = new Headers();
+    //headers.append("Content-type", "application/json");
+    headers.append("Authorizotion", "Bearer ice121232");
+
+    const formData = new FormData();
+    formData.append("userId", data.userId);
+    formData.append("title", data.title);
+    formData.append("body", data.body);
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      //body: JSON.stringify(data),
+      body: formData,
+      headers,
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        if (res.ok && res.status === 200) {
+          return res.json();
+        }
+      })
+      .then((data) => setUsers(data))
+      .catch((err) => console.log(err));
+
+    addPost({
+      userId: 1,
+      title: "Sample Post",
+      body: "Post Content",
+    });
+  }, []);
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("avatar", avatar);
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: formData,
+    });
+  };
+
   return (
     <>
       <form onSubmit={submitHandle}>
